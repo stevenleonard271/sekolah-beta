8c8d13902cf5365e464cabca5548d7df3506b987
stevenleonard271 <stevenleonard271@gmail.com>
2022-09-24 09:48:04 +0700

    perbaikan tugas asset widget

--- Tugas/latihan_asset_widget/lib/main.dart
+++ Tugas/latihan_asset_widget/lib/main.dart
@@ -26,69 +26,75 @@ class MyApp extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Row(
-                    children: const [
-                      Text(
-                        'Hi, David',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w700),
-                      ),
-                      Spacer(),
-                      ClipOval(
-                        child: Image(
-                            height: 75,
-                            width: 75,
-                            image: NetworkImage(
-                                'https://awsimages.detik.net.id/community/media/visual/2019/02/19/42393387-9c5c-4be4-97b8-49260708719e.jpeg?w=750&q=90')),
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(50.0),
+                    child: Row(
+                      children: const [
+                        Text(
+                          'Hi, David',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w700),
+                        ),
+                        Spacer(),
+                        ClipOval(
+                          child: Image(
+                              height: 75,
+                              width: 75,
+                              image: NetworkImage(
+                                  'https://awsimages.detik.net.id/community/media/visual/2019/02/19/42393387-9c5c-4be4-97b8-49260708719e.jpeg?w=750&q=90')),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-            Container(
-              margin: EdgeInsets.fromLTRB(150, 200, 150, 200),
-              width: 500,
-              height: 300,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  const Text(
-                    'Halo Button',
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.w700),
-                  ),
-                  const Text(
-                    'Pencet saya',
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.w700),
-                  ),
-                  SizedBox(
-                    width: 250,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.amber,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(Icons.abc),
-                          SizedBox(width: 10),
-                          Text(
-                            'Pesan Test sekarang',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w700),
-                          ),
-                        ],
-                      ),
+            Center(
+              child: Container(
+                // margin: EdgeInsets.fromLTRB(150, 200, 150, 200),
+                height: 200,
+                width: 400,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Text(
+                      'Halo Button',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.w700),
                     ),
-                  )
-                ],
+                    const Text(
+                      'Pencet saya',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(
+                      width: 250,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.amber,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(Icons.abc),
+                            SizedBox(width: 10),
+                            Text(
+                              'Pesan Test sekarang',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
